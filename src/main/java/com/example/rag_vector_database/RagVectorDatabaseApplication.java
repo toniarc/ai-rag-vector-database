aba7b0839728445c5b69ab128d8b90e2511c70af
Antonio Junior <anttonio.junior@prodepa.pa.gov.br>
2024-09-04 17:38:37 -0300

    first commit

---
+++ src/main/java/com/example/rag_vector_database/RagVectorDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.rag_vector_database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagVectorDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagVectorDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rag_vector_database/RagVectorDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rag_vector_database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagVectorDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
